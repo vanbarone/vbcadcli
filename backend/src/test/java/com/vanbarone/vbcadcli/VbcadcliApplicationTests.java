ac961a00f8960456d9d914dcbe1950690ccf8428
Vanessa Barone <vanbarone.milani@gmail.com>
2022-05-24 16:03:59 -0300

    Criação do projeto

---
+++ backend/src/test/java/com/vanbarone/vbcadcli/VbcadcliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vanbarone.vbcadcli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VbcadcliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
